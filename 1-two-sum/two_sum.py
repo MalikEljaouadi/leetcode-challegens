56031ca07d0793b55ec4d24dd2e4e9cdbea25649
Malik Eljaouadi <malik.eljaouadi@wefox.com>
2023-10-16 01:55:26 +0200

    ✨ 1-two-sum python solutions: Brute force + Optimized solutions

---
+++ 1-two-sum/two_sum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def twoSum_1(self, nums, target):
+        """
+        Brute force solution
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+    def twoSum_2(self, nums, target):
+        """
+        Optimized solution
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        latest_indexes = {}
+        for i in range(len(nums)):
+            if target - nums[i] in nums:
+                latest_indexes[i] = nums.index(target - nums[i])
+        return [latest_indexes.keys()[-1], latest_indexes.values()[-1]]
---
+++ Readmd.md
@@ -0,0 +1,9 @@
+# Intoduction
+
+This is a repo containing my answers for the `75 Must Do Leetcode` list of challegens.
+
+I will try for ecah challenge to give the brute force solution and the most optimized one.
+
+I will use mainly `Python` for solving the challenges but I will try to use `Rust` also.
+
+The blind 75 Must Do Leetcode: https://leetcode.com/list/xi4ci4ig/ 
